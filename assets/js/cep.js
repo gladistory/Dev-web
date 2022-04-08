e497db638507a2fa8c999dd7ccc4712cf8c04820
Vinicius de Souza Santos <69911251+gladistory@users.noreply.github.com>
2022-04-08 14:00:31 -0300

    Add files via upload

---
+++ assets/js/cep.js
@@ -0,0 +1,361 @@
+const bairros = [{
+    name: 'Abraão',
+    date: 'terça-feira',
+    hour: '19h',
+    obs: 'seletiva flex só de vidros com contentor modelo europeu verde SEX, manhã'
+}, {
+    name: 'Agronômica',
+    date: 'terça-feira',
+    hour: '19h',
+    obs: ' '
+}, {
+    name: 'Alto Ribeirão',
+    date: 'sábado',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Armação do Pântano do Sul',
+    date: 'quinta-feira',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Balneário dos Açores',
+    date: 'quinta-feira',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Barra da Lagoa',
+    date: 'quinta-feira',
+    hour: '7h',
+    obs: 'Na lixeira costa da lagoa'
+}, {
+    name: 'Fortaleza da Barra',
+    date: 'quinta-feira',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Lagoa da Conceição',
+    date: 'Canto da Lagoa = Segunda-feira.<br>Canto dos Araçás e Centrinho = Terça-feira.',
+    hour: '7h',
+    obs: 'O horário é referente a lixeira costa da lagoa. Nos trapiches ocorre as quartas-feiras, 21h'
+}, {
+    name: 'Bom Abrigo',
+    date: 'quinta-feira',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Cachoeira do Bom Jesus',
+    date: 'terça-feira',
+    hour: '19h',
+    obs: 'TER, 7h Seletiva flex só de vidro'
+}, {
+    name: 'Vargem do Bom Jesus',
+    date: 'terça-feira',
+    hour: '19h',
+    obs: 'TER, 7h Seletiva flex só de vidro'
+}, {
+    name: 'Cacupé',
+    date: 'quarta-feira',
+    hour: '7h',
+    obs: 'seletiva flex só de vidros contentor modelo europeu verde QUI, manhã'
+}, {
+    name: 'Caminho dos Açores',
+    date: 'quarta-feira',
+    hour: '7h',
+    obs: 'seletiva flex só de vidros contentor modelo europeu verde QUI, manhã'
+}, {
+    name: 'Caiacanga',
+    date: 'sábado',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Caieira da Barra do Sul',
+    date: 'sábado',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Campeche',
+    date: 'sexta-feira',
+    hour: '7h',
+    obs: 'Campeche Central Rodovia SC-405 até Trevo do Erasmo - terça-feira, 7h'
+}, {
+    name: 'Canto do Lamim',
+    date: 'segunda-feira',
+    hour: '19h',
+    obs: 'QUA, 7h Seletiva flex só de vidro'
+}, {
+    name: 'Canasvieiras',
+    date: 'quinta-feira',
+    hour: '19h',
+    obs: 'TER, 7h Seletiva flex só de vidro'
+}, {
+    name: 'Capoeiras',
+    date: 'terça-feira',
+    hour: '19h',
+    obs: ' '
+}, {
+    name: 'Carianos',
+    date: 'quarta-feira',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Carvoeira',
+    date: 'quarta-feira',
+    hour: '19h',
+    obs: 'Na quarta não tem convencional'
+}, {
+    name: 'Centro',
+    date: 'Calçadões = Todos os dias exceto sábado.<br>Área residencial = Domingo, quarta e sexta-feira.<br>Av. Mauro Ramos = Quarta-feira.<br>Morro da Cruz = Terça-feira',
+    hour: '19h',
+    obs: 'Na quarta feira não tem convencional'
+}, {
+    name: 'Coloninha',
+    date: 'segunda e sexta-feira',
+    hour: '19h',
+    obs: ' '
+}, {
+    name: 'Coqueiros',
+    date: 'Via gastronômica = domingo e quarta-feira.<br>Bairro = Domingo e Quinta-feira.',
+    hour: '19h',
+    obs: ' '
+}, {
+    name: 'Córrego Grande',
+    date: 'quarta-feira e sábado',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Costa de Dentro',
+    date: 'quinta-feira',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Costa da Lagoa',
+    date: 'quarta-feira',
+    hour: '21h',
+    obs: 'Nos trapiches'
+}, {
+    name: 'Costeira do Pirajubaé',
+    date: 'quarta-feira',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Daniela',
+    date: 'sexta-feira',
+    hour: '19h',
+    obs: 'TER, 7h Seletiva flex só de vidro'
+}, {
+    name: 'Estreito',
+    date: 'quarta-feira',
+    hour: '19h',
+    obs: 'Área comercial e Balneário do Estreito = quarta-feira.<br>Bairro de Fátima = Sexta-feira.'
+}, {
+    name: 'Ingleses',
+    date: 'Lado Norte Balneário Gaivotas, Centrinho e SC-403 = Quarta-feira. <br>Lado Sul Centrinho e Dom João Becker, Rua Tubarões e transversais = Quinta-feira',
+    hour: 'Quarta-feira = 19h ; Quinta-feira = 7h',
+    obs: ' '
+}, {
+    name: 'Itacorubi',
+    date: 'segunda e sexta-feira',
+    hour: '19h',
+    obs: ' '
+}, {
+    name: 'Itaguaçu',
+    date: 'quinta-feira',
+    hour: '19h',
+    obs: 'Seletiva flex só de vidros com contentor modelo europeu verde SEX, manhã'
+}, {
+    name: 'Jardim Anchieta',
+    date: 'segunda e sexta-feira',
+    hour: '7h',
+    obs: ''
+}, {
+    name: 'Jardim Atlântico',
+    date: 'segunda e sexta-feira',
+    hour: '19h',
+    obs: ' '
+}, {
+    name: 'João Paulo',
+    date: 'domingo e quinta-feira',
+    hour: '19h',
+    obs: ' '
+}, {
+    name: 'José Mendes',
+    date: 'quarta-feira',
+    hour: '19h',
+    obs: 'Não tem convencional. '
+}, {
+    name: 'Jurerê Tradicional',
+    date: 'segunda-feira',
+    hour: '19h',
+    obs: 'TER, 7h Seletiva flex só de vidro'
+}, {
+    name: 'Jurerê Internacional',
+    date: 'sexta-feira',
+    hour: '19h',
+    obs: 'TER, 7h Seletiva flex só de vidro'
+}, {
+    name: 'Lagoinha do Norte',
+    date: 'terça-feira',
+    hour: '19h',
+    obs: ' '
+}, {
+    name: 'Monte Verde',
+    date: 'Sábado',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Morro das Pedras',
+    date: 'Sábado',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Areias do Campeche',
+    date: 'Sábado',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Alto Riberão',
+    date: 'Sábado',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Pantanal',
+    date: 'quarta-feira',
+    hour: '19h',
+    obs: ' '
+}, {
+    name: 'Ufsc',
+    date: 'quarta-feira',
+    hour: '19h',
+    obs: ' '
+}, {
+    name: 'Pântano do Sul',
+    date: 'quinta-feira',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Parque São Jorge',
+    date: 'quarta-feira e sábado',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Ponta das Canas',
+    date: 'terça-feira',
+    hour: '19h',
+    obs: 'TER, 7h Seletiva flex só de vidro'
+}, {
+    name: 'Praia Brava',
+    date: 'terça-feira',
+    hour: '19h',
+    obs: 'TER, 7h Seletiva flex só de vidro'
+}, {
+    name: 'Ratones',
+    date: 'quarta-feira',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Ribeirão da Ilha',
+    date: 'Sábado',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Rio Tavares',
+    date: 'terça-feira',
+    hour: '7h',
+    obs: 'Está incluso região da Lagoinha Pequena, Fazenda do Rio Tavares e Campeche Central (SC-405 até Trevo do Erasmo)'
+}, {
+    name: 'Rio Vermelho',
+    date: 'segunda-feira',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Saco dos Limões',
+    date: 'quarta-feira',
+    hour: '19h',
+    obs: 'Vila operária '
+}, {
+    name: 'Saco Grande',
+    date: 'Sábado',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Sambaqui',
+    date: 'quinta-feira',
+    hour: '19h',
+    obs: 'seletiva flex só de vidros contentor modelo europeu verde QUI, manhã'
+}, {
+    name: 'Santa Mônica',
+    date: 'segunda-feira e sexta-feira',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Santinho',
+    date: 'quinta-feira',
+    hour: '7h',
+    obs: 'Inclui a SC-401'
+}, {
+    name: 'Santo Antônio de Lisboa',
+    date: 'quinta-feira',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Serrinha',
+    date: 'quarta-feira',
+    hour: '19h',
+    obs: ' '
+}, {
+    name: 'Sítio Capivari',
+    date: 'terça-feira',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Solidão',
+    date: 'quinta-feira',
+    hour: '7h',
+    obs: ' '
+}, {
+    name: 'Tapera',
+    date: 'quarta-feira',
+    hour: '7h',
+    obs: 'E base aérea'
+}, {
+    name: 'Trindade',
+    date: 'quarta-feira',
+    hour: '19h',
+    obs: 'Exceto parte alta do bairro.'
+}, {
+    name: 'Vargem Grande',
+    date: 'quarta-feira',
+    hour: '19h',
+    obs: ' '
+}, {
+    name: 'Vargem Pequena',
+    date: 'segunda-feira',
+    hour: '7h',
+    obs: ' ',
+}];
+
+function meu_callback(conteudo) {
+    const showBairro = document.getElementById('valueBairroEl');
+    let bairro = conteudo.bairro;
+
+    let colectBairro = bairros.find(b => b.name == bairro);
+
+    if (colectBairro) {
+        showBairro.innerHTML = `<small class="sucesscep"><i class="far fa-check-square"></i> CEP encontrado!</small> <br> A coleta seletiva no bairro <strong>${colectBairro.name}</strong> é:<br><br><strong>Dia:</strong> ${colectBairro.date}<br><strong>Horas:</strong> ${colectBairro.hour}<br>Atenção: ${colectBairro.obs}`
+    } else {
+        showBairro.innerHTML = `<small class="failcep"><i class="fas fa-exclamation-triangle"></i> Ops!!!</small> <br>Não encontramos o seu cep ou a coleta seletiva no seu bairro, digite o cep de um bairro mais próximo`;
+    }
+
+
+};
+
+function pesquisacep(valor) {
+    let cep = valor;
+    //Cria um elemento javascript.
+    let script = document.createElement('script');
+    //Sincroniza com o callback.
+    script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=meu_callback';
+    //Insere script no documento e carrega o conteúdo.
+    document.body.appendChild(script);
+};
